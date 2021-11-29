e0abf2b502026db6b8523ca8d7652b4b85c67ba3
Ariso Aetherium <arisoaetherium@gmail.com>
2021-11-29 18:30:09 -0500

    Update - Ejercicio soledad: Resuelto el soledad ejercicio

---
+++ Ejercicio13.py
@@ -0,0 +1,52 @@
+# Hecho por Luis Mendoza
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Cargado de CSV
+
+# Profe, modifique el filepath para evitar el error que impide cargar el documento correctamente.
+# Debe colocar la dirección en carpeta COMPLETA del archivo para que cargue
+
+filePathCSV = "D:/Shared/Mis Cosas de la Universidad/9 - Semestre/Inteligencia Computacional/Tercer Corte/30-11-2021/Archivos/covid_22_noviembre.csv"
+datosCovid = pd.read_csv(filePathCSV)
+
+# Normalización de los datos datos
+
+
+datosCovid[datosCovid['Sexo']=='F'].shape[0]
+datosCovid[datosCovid['Sexo']=='M'].shape[0]
+datosCovid[datosCovid['Sexo']=='f'].shape[0]
+datosCovid[datosCovid['Sexo']=='m'].shape[0]
+
+datosCovid.groupby('Sexo').size()
+datosCovid.groupby('Estado').size()
+
+datosCovid['Estado'].replace('LEVE','leve', inplace=True)
+datosCovid['Estado'].replace('moderado','Moderado', inplace=True)
+datosCovid['Estado'].replace('leve','Leve', inplace=True)
+
+datosCovid['Tipo de contagio'].replace('En ESTUDIO','En Estudio', inplace=True)
+
+datosCovid['Tipo de contagio'].replace('relacionado','Relacionado', inplace=True)
+
+datosCovid['Ubicación del caso'].replace('CASA','Casa', inplace=True)
+
+datosCovid['Tipo de contagio'].replace('En estudio','En Estudio', inplace=True)
+
+datosCovid['Tipo de contagio'].replace('RELACIONADO','relacionado', inplace=True)
+
+
+datosCovid['Sexo'].replace('m','M', inplace=True)
+datosCovid['Sexo'].replace('f','F', inplace=True)
+
+# Ejercicio 13 - Mayor a menor los 10 departamentos con mas casos de recuperados
+
+rec = datosCovid[datosCovid['Estado'] == 'Recuperado']
+rec_dep = fallecidos.groupby('Nombre departamento').size()
+rec_dep_ord = fall_dep.sort_values(ascending=False).head(10)
+print("-------------------")
+print("Los 10 departamentos con mas Recuperados: {}".format(dep_cont))
+print("-------------------")
